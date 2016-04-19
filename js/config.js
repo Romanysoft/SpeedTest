d98b2dd17af1b07bb7c624359362d1b32f3cec17
iansun <lauer3912@gmail.com>
2016-04-19 21:06:29 +0800

    更新

--- js/config.js
+++ js/config.js
@@ -5,10 +5,10 @@
         documentTitle:"SpeedTest By Romanysoft",
         supportPlatforms:["Mac"],
         googleUA:"UA-43238484-10",
-        gitHome:"https://github.com/Romanysoft/SpeedTest",
-        reportIssueUrl:"https://github.com/Romanysoft/SpeedTest/issues",
-        changeLogUrl:"https://github.com/Romanysoft/SpeedTest/wiki/Changelog",
-        wikiUrl:"https://github.com/Romanysoft/SpeedTest/wiki",
+        gitHome:"//github.com/Romanysoft/SpeedTest",
+        reportIssueUrl:"//github.com/Romanysoft/SpeedTest/issues",
+        changeLogUrl:"//github.com/Romanysoft/SpeedTest/wiki/Changelog",
+        wikiUrl:"//github.com/Romanysoft/SpeedTest/wiki",
         romanysoftHome:"//www.romanysoft.com",
         copyright:"Copyright © " + (new Date()).getFullYear() + " Romanysoft LAB."
     }
