dbd9228abd84494f441ae7931056efb84898eb02
iansun <lauer3912@gmail.com>
2017-12-14 13:57:24 +0800

    update

--- js/config.js
+++ js/config.js
@@ -1,15 +1,15 @@
 (function () {
     window.RTYConfig = {
-        appName:"Romanysoft Speed19X",
+        appName:"SpeedTest",
         appID:"com.romanysoft.app.macos.speedtest",
-        documentTitle:"Romanysoft Speed19X",
+        documentTitle:"SpeedTest",
         supportPlatforms:["Mac","Windows"],
         googleUA:"UA-76676167-6",
         gitHome:"//github.com/Romanysoft/SpeedTest",
         reportIssueUrl:"//github.com/Romanysoft/SpeedTest/issues",
         changeLogUrl:"//github.com/Romanysoft/SpeedTest/wiki/Changelog",
         wikiUrl:"//github.com/Romanysoft/SpeedTest/wiki",
-        romanysoftHome:"//www.romanysoft.com",
+        romanysoftHome:"https://aws.romanysoft.com",
         copyright:"Copyright © " + (new Date()).getFullYear() + " Romanysoft LAB."
     }
     
